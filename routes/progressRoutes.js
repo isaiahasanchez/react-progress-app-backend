e7d13ba69a50c4061069c4634a45133a5d8b4f0e
Isaiah Sanchez <isaiahsanchez.ca@gmail.com>
2023-09-27 13:15:46 -0700

    added logout function

--- routes/progressRoutes.js
+++ routes/progressRoutes.js
@@ -1,27 +1,41 @@
-const express = require('express');
-const passport = require('passport')
-const progressController = require('../controllers/progressController');
+const express = require("express");
+const passport = require("passport");
+const progressController = require("../controllers/progressController");
 const router = express.Router();
-const { ensureAuthenticated } = require('../middleware/authMiddleware');
-
-
-router
-    .route('/posts')
-    .get(ensureAuthenticated, progressController.getAllPosts)
-    .post(ensureAuthenticated, progressController.createNewPost); // Updated for consistency
+const { ensureAuthenticated } = require("../middleware/authMiddleware");
 
 router
-    .route('/posts/:id')
-    .get(ensureAuthenticated, progressController.getOnePost)
-    .delete(ensureAuthenticated, progressController.deletePost)
-    .put(ensureAuthenticated, progressController.editPost); // Updated for consistency
-
-router 
-    .route('/register')
-    .post(progressController.createNewUser)
-
+  .route("/posts")
+  .get(ensureAuthenticated, progressController.getAllPosts)
+  .post(ensureAuthenticated, progressController.createNewPost); // Updated for consistency
 
 router
-    .route('/login')
-    .post(passport.authenticate('local'), progressController.login)
+  .route("/posts/:id")
+  .get(ensureAuthenticated, progressController.getOnePost)
+  .delete(ensureAuthenticated, progressController.deletePost)
+  .put(ensureAuthenticated, progressController.editPost); // Updated for consistency
+
+router.route("/register").post(progressController.createNewUser);
+
+router
+  .route("/login")
+  .post(passport.authenticate("local"), progressController.login);
+
+  router.post("/logout", (req, res) => {
+    req.logout((err) => {
+      if(err) {
+        return res.status(500).json({ message: "Could not log out, please try again." });
+      }
+      
+      // if using sessions, remember to also clear the session
+      req.session.destroy((err) => {
+        if(err) {
+          return res.status(500).json({ message: "Could not clear session, please try again." });
+        }
+        
+        res.status(200).json({ message: "Logged out successfully" });
+      });
+    });
+  });
+  
 module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-const express = require ('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-require('dotenv').config()
-const progressRoutes = require('./routes/progressRoutes')
-const session = require('express-session')
-const passport = require('passport')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+const progressRoutes = require('./routes/progressRoutes');
+const session = require('express-session');
+const passport = require('passport');
 const passportConfig = require('./middleware/passport');
-const app = express()
-app.use(express.json())
+
+const app = express();
+app.use(express.json());
+
 app.use(cors({
     origin: 'http://localhost:3000', // replace with your application's URL
     credentials: true,
-}))
+}));
 
 // Initializing Session
 app.use(session({ secret: process.env.SESSION_KEY, resave: false, saveUninitialized: false }));
@@ -20,15 +22,13 @@ app.use(session({ secret: process.env.SESSION_KEY, resave: false, saveUninitiali
 app.use(passport.initialize());
 app.use(passport.session());
 
+// use your routes after initializing passport and session
+app.use('/', progressRoutes);
 
-
-app.use('/', progressRoutes)
-
+// connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log('connected do DB'))
-.catch(error => console.log(error.message))
+    .then(() => console.log('connected to DB'))
+    .catch(error => console.log(error.message));
 
-
-
-app.listen(5500, () => console.log('Server started on port 5500'))
-
+// start the server
+app.listen(5500, () => console.log('Server started on port 5500'));
