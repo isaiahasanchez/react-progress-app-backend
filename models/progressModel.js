71d1b56620db54478a13aaf7807e5f9713ec90fb
Isaiah Sanchez <isaiahsanchez.ca@gmail.com>
2024-02-01 17:43:54 -0800

    new schema

--- models/progressModel.js
+++ models/progressModel.js
@@ -1,11 +1,24 @@
 const mongoose = require('mongoose');
 
+// Define a sub-schema for individual sets
+const SetSchema = new mongoose.Schema({
+  weight: Number,
+  reps: Number,
+});
+
+// Define a sub-schema for the workout on a particular date
+const WorkoutSchema = new mongoose.Schema({
+  date: Date,
+  set: [SetSchema],
+});
+
 const exerciseSchema = new mongoose.Schema({
-  exercise: String,
+  exerciseName: String,
   equipment: String,
   image: String,
   // Change sets to an array once frontend feature branch finishes
-  sets: String,
+  workouts: [WorkoutSchema],
+  editMode: Boolean,
   lastDateEdited: {
     type: Date,
     default: Date.now,
